62d9b5b9cde99ac444e4123583585db797671ebf
v.pastukh <v.pastukh@foodtech.team>
2024-03-31 23:50:17 +0300

    fix

--- btn.js
+++ btn.js
@@ -2,9 +2,8 @@ window.onscroll = function showHeader(){
     let cont = document.querySelector('.cont');
 
     if(window.scrollY > 1000){
-        cont.classList.add('cont-fixed');
-    } else{
-        cont.classList.remove('cont-fixed');
-
+        cont.style.display = "block";
+    } else {
+        cont.style.display = "none";
     }
 }

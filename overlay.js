0de9638e9142b2d3a443c0f1ae4b19d14b6af921
v.pastukh <v.pastukh@foodtech.team>
2024-03-31 13:43:53 +0300

    fixes

--- overlay.js
+++ overlay.js
@@ -19,3 +19,27 @@ document.addEventListener('DOMContentLoaded', function() {
         this.style.display = 'none';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sliderItems = document.querySelectorAll('.slider-item');
+    const overlay = document.querySelector('.overlay');
+
+    sliderItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            const iframe = this.querySelector('iframe');
+            if (iframe) {
+                overlay.innerHTML = ''; // Очистимо оверлей
+                overlay.appendChild(iframe.cloneNode(true)); // Додамо клон iframe в оверлей
+                overlay.style.display = 'flex'; // Показуємо оверлей
+                event.stopPropagation(); // Зупиняємо подальше спливання події
+            }
+        });
+    });
+
+    overlay.addEventListener('click', function(event) {
+        if (event.target.tagName !== 'IFRAME') { // Перевіряємо, чи клік був на iframe
+            this.innerHTML = ''; // Очищуємо оверлей при кліку поза iframe
+            this.style.display = 'none'; // Приховуємо оверлей
+        }
+    });
+});
